f6ba5a0100c326994d241a3dc70e4437ebc59a1c
petar-antic <antic.petar994@gmail.com>
2021-09-30 10:02:52 +0200

    Tab width and gitignore files

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ const warning = document.querySelector('.error');
 const button = document.querySelector('button');
 
 button.addEventListener('click', (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-  if (!email.value) {
-    warning.innerHTML = 'Oops! Please add your email';
-  } else if (!email.value.match(pattern)) {
-    warning.innerHTML = 'Oops! Please check your email';
-  } else {
-    warning.innerHTML = '&nbsp;';
-  }
+    if (!email.value) {
+        warning.innerHTML = 'Oops! Please add your email';
+    } else if (!email.value.match(pattern)) {
+        warning.innerHTML = 'Oops! Please check your email';
+    } else {
+        warning.innerHTML = '&nbsp;';
+    }
 
-  email.value = '';
+    email.value = '';
 });
 
 document.addEventListener('click', (e) => {
-  if (e.target.tagName !== 'BUTTON' && e.target.tagName !== 'INPUT') {
-    warning.innerHTML = '&nbsp;';
-  }
+    if (e.target.tagName !== 'BUTTON' && e.target.tagName !== 'INPUT') {
+        warning.innerHTML = '&nbsp;';
+    }
 });
